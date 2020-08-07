dc58f71cf69cfe125faa6d2610899033563fe5e2
Jeeves <Parsashowkati@gmail.com>
2020-08-07 17:15:18 +0430

    Update and rename main.py to rasp.py

---
+++ rasp.py
@@ -0,0 +1,10 @@
+import time, requests
+from board import SCL, SDA
+import busio
+from adafruit_seesaw.seesaw import Seesaw 
+ss = Seesaw(busio.I2C(SCL, SDA), addr=0x36)
+
+heroku = "" #Enter your heroku app website here
+while True:
+    requests.post(heroku, data{"temp": ss.get_temp(), "moisture": ss.moisture_read()})
+    time.sleep(1)
