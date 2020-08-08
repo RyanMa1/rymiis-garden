d75be060497e170a104288badcc91a7b8852ab1c
Jeeves <Parsashowkati@gmail.com>
2020-08-08 04:33:07 +0430

    Update rasp.py

--- rasp.py
+++ rasp.py
@@ -4,7 +4,7 @@ import busio
 from adafruit_seesaw.seesaw import Seesaw 
 ss = Seesaw(busio.I2C(SCL, SDA), addr=0x36)
 
-heroku = "" #Enter your heroku app website here
+heroku = "" #Enter your heroku app URL here
 while True:
-    requests.post(heroku, data{"temp": ss.get_temp(), "moisture": ss.moisture_read()})
+    requests.post(heroku, data={"temp": ss.get_temp(), "moisture": ss.moisture_read()})
     time.sleep(1)
